26141f25946746ded28dba8d348d312566b92793
swethabala058 <swethabala1407@gmail.com>
2025-10-04 19:15:39 +0530

    commit-2

--- run_compression_tool.bat
+++ run_compression_tool.bat
@@ -1,6 +1,9 @@
 @echo off
 REM Batch file to run the CompressionTool JavaFX application using Maven
 
+REM Set Maven path
+set PATH=%PATH%;maven\apache-maven-3.8.8\bin
+
 REM Run the application using Maven
 mvn clean javafx:run
 
